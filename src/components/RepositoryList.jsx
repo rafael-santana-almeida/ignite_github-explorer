bb6f046351c5285e24b4e91d8de317b1d14599ce
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-01-09 12:42:57 -0300

    Create RespositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição</p>
+
+          <a href="">Acessar repositóro</a>
+        </li>
+
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
